24f011f9766c2dc0a45d72d1f275136a38d1b9de
Safaa <safa3.1998@hotmail.com>
2021-02-09 15:57:00 +0300

    fix sweet alert for read more button

---
+++ GaithAlQuraniProject/Models/RegistrationForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GaithAlQuraniProject.Models
+{
+    public class RegistrationForm
+    {
+        public int Id { get; set; }
+
+        //many to one
+        public string Name { get; set; }
+        //country
+        //key
+        public string phoneNumber { get; set; }
+
+
+        // how many Juzu memorized
+        //drop down (inside Ksa (show phone key) or outside) asp.net course
+
+        //Reywayah
+
+
+    }
+}
